e8f7aba73cbd35551527bc6bd011dc04fa51423e
Sachin Muralidhara <sachin.muralidhara@colorado.edu>
2015-10-26 17:32:36 -0600

    "cleaning the html"

--- trainingSet.py
+++ trainingSet.py
@@ -3,19 +3,41 @@ import itertools
 import urllib2
 from bs4 import BeautifulSoup
 
-i = 2
+def cleanHtml():
 
-try:
-	with open('BBC_data.csv', 'rb') as myFile:
-		reader = csv.reader(myFile)
+	i = 3
+
+	try:
+		with open('Reuter_data.csv', 'rb') as myFile:
+		# reader = csv.reader(myFile)
+		dialect = csv.Sniffer().sniff(myFile.read(), delimiters = ";,")
+
+		reader = csv.reader(myFile, dialect)
+
 		# row_count = sum(1 for row in reader)
 		# print row_count
 		for row in itertools.islice(reader, 2, 3):
 			link = row[0]
+
 			page = urllib2.urlopen(link)
-			print page.info()
+
 			soup = BeautifulSoup(page.read())
-			print soup.prettify()
+
+			for script in soup(["script", "style"]):
+				script.extract()
+
+			# get text
+			text = soup.get_text()
+
+			# break into lines and remove leading and trailing space on each
+			lines = (line.strip() for line in text.splitlines())
+			# break multi-headlines into a line each
+			chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+			# drop blank lines
+			text = '\n'.join(chunk for chunk in chunks if chunk)	 
+
+			print text
+
 			page.close()
-except:
-	pass			
+	except:
+		pass			
