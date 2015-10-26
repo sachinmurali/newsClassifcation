d5ec50f3913526e45d942388ea731343f9f0460c
Sachin Muralidhara <sachin.muralidhara@colorado.edu>
2015-10-26 17:49:13 -0600

    "This script parses only text from html"

--- trainingSet.py
+++ trainingSet.py
@@ -3,20 +3,17 @@ import itertools
 import urllib2
 from bs4 import BeautifulSoup
 
-def cleanHtml():
+# i = 3
 
-	i = 3
-
-	try:
-		with open('Reuter_data.csv', 'rb') as myFile:
+try:
+	with open('Reuter_data.csv', 'rb') as myFile:
 		# reader = csv.reader(myFile)
 		dialect = csv.Sniffer().sniff(myFile.read(), delimiters = ";,")
-
+		myFile.seek(0)
 		reader = csv.reader(myFile, dialect)
-
 		# row_count = sum(1 for row in reader)
 		# print row_count
-		for row in itertools.islice(reader, 2, 3):
+		for row in itertools.islice(reader, 3, 4):
 			link = row[0]
 
 			page = urllib2.urlopen(link)
@@ -39,5 +36,5 @@ def cleanHtml():
 			print text
 
 			page.close()
-	except:
-		pass			
+except:
+	pass			
